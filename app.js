b767805b217980ed420675b68979dfc8bf12545b
Johny <tranceboy_johny@Johnys-MacBook-Pro.local>
2018-03-25 17:37:54 -0400

    code refactoring

--- app.js
+++ app.js
@@ -9,40 +9,6 @@
         this.Products = gems;
     });
 
-    app.controller('GalleryController', function(){
-        this.current = 0;
-
-        this.setCurrent = function(val){
-            this.current = val || 0;
-        };
-    });
-
-    app.controller('PanelController', function(){
-        this.tab = 1;
-
-        this.selectTab = function(val){
-            this.tab = val;
-        }
-
-        this.isSelected = function(tab){
-            return this.tab === tab;
-        }
-    });
-
-    app.controller("ReviewController", function(){
-        this.review = {};
-
-        this.addReview = function(product){
-            // passing the current instance of product 
-            // and then adding the review object to the array of reviews
-            product.reviews.push(this.review);
-
-            // after the review has been attached to the array
-            // we reset the review so our form clears up!!!
-            this.review = {};
-        }
-    });
-
     app.directive('productTitle', function(){
         return {
             restrict: 'E', // E = element
@@ -58,11 +24,43 @@
     });
 
     app.directive('productReviews', function(){
-        return { restrict: 'E', templateUrl: 'product-reviews.html'};
+        return { 
+            restrict: 'E', 
+            templateUrl: 'product-reviews.html',
+            controller: function(){
+                this.review = {};
+                
+                this.addReview = function(product){
+                    // passing the current instance of product 
+                    // and then adding the review object to the array of reviews
+                    product.reviews.push(this.review);
+        
+                    // after the review has been attached to the array
+                    // we reset the review so our form clears up!!!
+                    this.review = {};
+                }
+            },
+            controllerAs: 'reviewCtrl'
+        };
     });
 
     app.directive('productSpecs', function(){
         return { restrict: 'A', templateUrl: 'product-specs.html'};
+    });
+
+    app.directive('productGallery', function(){
+        return{
+            restrict: 'E',
+            templateUrl: 'product-gallery.html',
+            controller: function(){
+                this.current = 0;
+                
+                this.setCurrent = function(val){
+                    this.current = val || 0;
+                };
+            },
+            controllerAs: 'gallery'
+        };
     });
 
     app.directive('productPanel', function(){
