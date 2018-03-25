ea0eaefa33892e2ee9b2b00c7d4fc9c9daab29b9
Amandeep Johny Baryah <johnybaryah@hotmail.com>
2018-03-25 18:11:07 -0400

    services implementation

--- app.js
+++ app.js
@@ -1,86 +1,18 @@
 // Its good practice to wrap your entire app in a closure
 (function(){
     // this is a module called store - also known as your app name!
-    var app = angular.module('store', []);
+    var app = angular.module('store', ['store-directives']);
 
     // controller - this is where you'd define all the functions and values and app behavior
-    app.controller('StoreController', function(){
+    app.controller('StoreController', [$http, function($http){
         // power of JS! declare properties on the fly
-        this.Products = gems;
-    });
+        var store = this;
+        store.products = [];
 
-    app.directive('productTitle', function(){
-        return {
-            restrict: 'E', // E = element
-            templateUrl: 'product-info.html'
-        };
-    });
-
-    app.directive('productDesc', function(){
-        return {
-            restrict: 'E',
-            templateUrl: 'product-desc.html'
-        };
-    });
-
-    app.directive('productReviews', function(){
-        return { 
-            restrict: 'E', 
-            templateUrl: 'product-reviews.html',
-            controller: function(){
-                this.review = {};
-                
-                this.addReview = function(product){
-                    // passing the current instance of product 
-                    // and then adding the review object to the array of reviews
-                    product.reviews.push(this.review);
-        
-                    // after the review has been attached to the array
-                    // we reset the review so our form clears up!!!
-                    this.review = {};
-                }
-            },
-            controllerAs: 'reviewCtrl'
-        };
-    });
-
-    app.directive('productSpecs', function(){
-        return { restrict: 'A', templateUrl: 'product-specs.html'};
-    });
-
-    app.directive('productGallery', function(){
-        return{
-            restrict: 'E',
-            templateUrl: 'product-gallery.html',
-            controller: function(){
-                this.current = 0;
-                
-                this.setCurrent = function(val){
-                    this.current = val || 0;
-                };
-            },
-            controllerAs: 'gallery'
-        };
-    });
-
-    app.directive('productPanel', function(){
-        return {
-            restrict: 'E',
-            templateUrl : 'product-panel.html',
-            controller: function(){
-                this.tab = 1;
-                
-                this.selectTab = function(val){
-                    this.tab = val;
-                }
-        
-                this.isSelected = function(tab){
-                    return this.tab === tab;
-                }
-            },
-            controllerAs: 'panel'
-        };
-    });
+        $http.get('/products.json').success(function(data){
+            store.products = data;
+        });
+    }]);
 
     var gems = [
         {
