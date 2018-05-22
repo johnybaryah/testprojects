b6e711cf12b51247130c683bba58e64fa8504394
johnybaryah <johnybaryah@hotmail.com>
2018-05-22 10:00:44 -0400

    updates files

--- Gdpr_plugin.js
+++ Gdpr_plugin.js
@@ -9,21 +9,34 @@ class Eu_Cookie{
         this.val = val;
     }
 
-    static get(c){
-
+    get(){
+        var self = this;
         return new Promise(function(resolve, reject){
-            $.getScript(c.scriptLink).done(function(js){
-                resolve(typeof Cookies.get(c.name) !== "undefined");
+            $.getScript(self.scriptLink).done(function(js){
+                resolve(typeof Cookies.get(self.name) !== "undefined");
             });
         });
     }
+    
     set(){
+        var self = this;
         return new Promise(function(resolve, reject){
-            $.getScript(this.scriptLink).done(function(){
-                Cookies.set(this.name, this.val);
+            $.getScript(self.scriptLink).done(function(){
+                Cookies.set(self.name, self.val);
+
+                // cookie set should also make a server call to log ipAddress
+
                 resolve();
             });
         });
+    }
+
+    logInfo(){
+
+    }
+
+    toString(){
+        return this.name + ", " + this.val;
     }
 }
 
@@ -46,32 +59,6 @@ class IpStack{
     }
 }
 
-/*var IsCookieSet = () => {
-    return new Promise(function(resolve, reject){
-        $.getScript("https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js")
-        .done(function(js){            
-            resolve(typeof Cookies.get("_eu_wes") !== "undefined");
-        });
-    });    
-};
-
-var SetCookie = () => {
-    return new Promise(function(resolve, reject){
-        $.getScript("https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js")
-        .done(function(js){
-            Cookies.set("_eu_wes", 1);
-            resolve();
-        });
-    });    
-};
-
-var InjectScripts = (scripts) => {
-    scripts.forEach(element => {
-        console.log(element);
-        $.getScript("http://localhost/gdpr/" + element);
-    });
-};*/
-
 // if is europe => getLocation()
 // then find a cookie
 // if no cookie is found
@@ -79,36 +66,54 @@ var InjectScripts = (scripts) => {
 // if cookie is found then no popup return false; 
 
 class GDPR{
-    constructor(w, e, s){
+    constructor(w, e, s, i){
         this.mode = w;
         this.ScriptsToInject = e;
         this.appendTo = s;
+
+        // test
+        this.eu = i;
         
         this.checkParams();
 
-        this.modalContentUrl = "http://localhost/gdpr/terms.html";
-        this.alertContentUrl = "http://localhost/gdpr/alertText.html";
+        this.modalContentUrl = "https://jsgdps.azurewebsites.net/terms.html";
+        this.alertContentUrl = "https://jsgdps.azurewebsites.net/alertText.html";
     }
 
     Init(){
         var eucookie = new Eu_Cookie("_eu_wes", 1);
+        var self = this;
+        eucookie.get().then(function(cookieAlreadySet){
+            if (cookieAlreadySet){
+                self.injectScripts();
+                return;
+            } 
+            /* enable the code below when to use api - im disabling because I have site deployed on ssl and http call won't work*/
 
-        Eu_Cookie.get(eucookie).then(function(cookieAlreadySet, eucookie){
-            if (cookieAlreadySet) return;
-
-            var ip = new IpStack();
+            /*var ip = new IpStack();
             ip.getInfo().done(function(json){
                 if (!json.location.is_eu) {
-                    injectScripts();
+                    self.injectScripts();
                     return;
                 }
 
-                showConsent(eucookie);
-            });
+                self.showConsent(eucookie);
+            });*/
+
+            /* disable this when enabling the code above */
+            // if not eu
+            if (!self.eu){
+                self.injectScripts();
+                return;
+            }
+
+            self.showConsent(eucookie);
+            
         });
     }
 
     showConsent(eucookie){
+        var self = this;
         switch(this.mode){
             case "alert":
                 $.get(this.alertContentUrl).done(function(text){
@@ -121,9 +126,9 @@ class GDPR{
                 $.get(this.modalContentUrl).done((modal) => {
                     $('body').append(modal);                        
                     $("#savecookie").on('click', function(){
-                        SetCookie().then(function(){
+                        eucookie.set().then(function(){
                             $("#myModal").modal('hide');
-                            this.injectScripts();
+                            self.injectScripts();
                         });
                     });
                     $("#myModal").modal();
@@ -136,7 +141,7 @@ class GDPR{
 
     injectScripts(){
         this.ScriptsToInject.forEach(script => {
-            $.getScript("http://localhost/gdpr/" + script);
+            $.getScript("https://jsgdps.azurewebsites.net/" + script);
         });
     }
 
@@ -153,79 +158,3 @@ class GDPR{
         return false;
     }
 }
-
-/*var SetGdprTerms = (function(){
-    var allowedParams = ["redirect", "alert", "modal"];
-
-    var init = function (a, s, w){
-        if (allowedParams.indexOf(a) === -1) {
-            console.log("Invalid value passed as parameter for init menthod.");
-            return;
-        }
-
-        // init cookie class
-        var eucookie = new Eu_Cookie("_eu_wes", 1);
-
-        eucookie.get().then(function(cookieAlreadySet){
-            if (cookieAlreadySet) return;
-
-            var ip = new IpStack();
-            ip.getInfo().done(function(json){
-                if (!json.location.is_eu) {
-                    if (s !== null) InjectScripts(s);
-                    return;
-                }
-
-
-            });
-        });
-
-        IsCookieSet().then(function(r){            
-            if (r) return false;
-
-            var url = "http://api.ipstack.com/check";
-            var key = "88de7a4a65b399634e9291d9070aac3f";    
-            var params = {access_key : key};
-
-            $.ajax({
-                url,
-                data: params,
-                dataType: "jsonp",
-                type: "GET"
-            }).done(function(json){
-                //if (!json.location.ie_eu) return;
-                
-                //if (!json.location.is_eu) {
-                //    InjectScripts(s);
-                //    return;
-                //}
-
-                if (a === "alert"){
-                    $.get("http://localhost/gdpr/alertText.html").done(function(text){
-                        //alert("here");
-                        var newDiv = $('<div/>').addClass("alert alert-info newPrivacyAlert").attr("role","alert").append(text);
-                        w.prepend(newDiv);
-                        $("#okButton").on("click", function(){ SetCookie().then(function(){ $(".newPrivacyAlert").hide('slow'); });});
-                    });
-                }
-                else if (a === "modal"){
-                    $.get("http://localhost/gdpr/terms.html").done((modal) => {
-                        $('body').append(modal);                        
-                        $("#savecookie").on('click', function(){
-                            SetCookie().then(function(){
-                                $("#myModal").modal('hide');
-                                InjectScripts(s);
-                            });
-                        });
-                        $("#myModal").modal();
-                    });
-                }
-                else{
-                    window.location.replace("http://localhost/gdpr/index1.html");
-                }
-            });   
-        });
-    }
-
-    return { init: init }
-})();*/
